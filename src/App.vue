4ce648eab41a011ac0756f146f6312530a130d0f
wingland <illusion1125@gmail.com>
2018-03-23 18:00:14 +0800

    Init the project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{hello}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hello: 'Hello, World'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
